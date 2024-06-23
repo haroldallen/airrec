dc09ff112ac6c55a189f4873bac6ac18bd29cec5
Harold Allen <hmallen.cb@gmail.com>
2024-06-23 19:08:10 +0100

    filters

--- src/App.jsx
+++ src/App.jsx
@@ -4,34 +4,53 @@ import questions from "./assets/questions.json";
 
 import toast from "react-hot-toast";
 
-export default function App({nextScore}) {
+export default function App({nextScore, categories, types, showAnswers}) {
     const [itemIndex, setItemIndex] = useState(0);
     const [questionIndex, setQuestionIndex] = useState(0);
     const [multipleChoiceOptions, setMultipleChoiceOptions] = useState([]);
     const [completed, setCompleted] = useState([]);
 
+    const [selectedItems, setSelectedItems] = useState([]);
+
     useEffect(() => {
-        let newItemIndex = pickNewItem();
+        if (categories.length === 0 || types.length === 0) return;
+
+        let newSelectedItems = items.map((r)=>categories.includes(r.category)&&types.includes(r.type)?r:null).filter((r)=>r!==null);
+        if (newSelectedItems.length === 0) return;
+        setSelectedItems(newSelectedItems);
+
+        let newItemIndex = pickNewItem(false, newSelectedItems);
         
         let newQuestionIndex = Math.floor(Math.random() * questions.length);
         if (questions[newQuestionIndex].type === "multiple_choice") loadMultipleChoice(newItemIndex, questions[newQuestionIndex].answer);
         setQuestionIndex(newQuestionIndex);
-    }, []);
+    }, [categories]);
 
-    function pickNewItem(next=false) {
+    function pickNewItem(next=false, selectedItemsOverride=selectedItems) {
         let newItemIndex;
-        setItemIndex(newItemIndex);
 
         if (next) {
-            newItemIndex = itemIndex===items.length-1?0:itemIndex+1;
+            /*let currentSIIndex = selectedItemsOverride.indexOf(items[itemIndex].callsign);
+            newItemIndex = items.indexOf(selectedItemsOverride[currentSIIndex===selectedItemsOverride.length-1?0:currentSIIndex+1]);
+            console.log("items", items, "selected items", selectedItems, "sio[csii==sio.l-1?0:cstii+1]", selectedItemsOverride[currentSIIndex===selectedItemsOverride.length-1?0:currentSIIndex+1], "items[previous]", items[selectedItemsOverride[currentSIIndex===selectedItemsOverride.length-1?0:currentSIIndex+1]]);
+            */
+
+            let currentSTIndex = selectedItemsOverride.indexOf(items[itemIndex]);
+            newItemIndex = items.indexOf(selectedItemsOverride[currentSTIndex===selectedItemsOverride.length-1?0:currentSTIndex+1]);
+
         } else {
-            newItemIndex = Math.floor(Math.random() * items.length);
+            newItemIndex = items.indexOf(selectedItemsOverride[Math.floor(Math.random() * selectedItemsOverride.length)]);
         }
 
-        if (completed.includes(items[newItemIndex].callsign)) {
+        console.log(completed.length, selectedItemsOverride.length)
+        if (completed.length === selectedItemsOverride.length && completed.length !== 0) {
+            setCompleted([]);
+            toast.success("Completed all items in selected categories/types")
+        }
+        else if (completed.includes(items[newItemIndex].callsign)) {
             if (next) setItemIndex(newItemIndex);
             
-            newItemIndex = pickNewItem(next);
+            newItemIndex = pickNewItem(true);
         }
         setItemIndex(newItemIndex);
 
@@ -50,6 +69,7 @@ export default function App({nextScore}) {
                 next();
                 nextScore(true);
             } else {
+                if (showAnswers) toast("Correct answer: "+answer);
                 toast.error("Incorrect!");
                 next();
                 nextScore();
@@ -102,30 +122,33 @@ export default function App({nextScore}) {
     }
 
     return <>
-        <img className="image" src={items[itemIndex]?.image} />
-        <div className="question">
-            <div className="jsb">
-                <h1 className="title">{questions[questionIndex]?.title.replace("[type]", items[itemIndex]?.type)}</h1>
-                <p className={"difficulty "+(questions[questionIndex]?.difficulty)}>{questions[questionIndex]?.difficulty}</p>
-            </div>
-            { questions[questionIndex].type === "text" ? <>
-                <div className="inputs">
-                    {questions[questionIndex]?.inputs.map((r)=>(
-                        <input key={r.answer} type="text" placeholder={r.placeholder} />
-                    ))}
+        { categories.length === 0 || types.length === 0 || selectedItems.length === 0 ? <p>No items match selected filters</p>
+        : <>
+            <img className="image" src={items[itemIndex]?.image} />
+            <div className="question">
+                <div className="jsb">
+                    <h1 className="title">{questions[questionIndex]?.title.replace("[type]", items[itemIndex]?.type)}</h1>
+                    <p className={"difficulty "+(questions[questionIndex]?.difficulty)}>{questions[questionIndex]?.difficulty}</p>
                 </div>
-                <button onClick={()=>{check()}}>Check answers</button>
-            </> : questions[questionIndex].type === "multiple_choice" ? <>
-                <form className="inputs" name={Math.random()*1000}>
-                    {multipleChoiceOptions.map((r, index)=>(
-                        <div key={index} className="option">
-                            <input name="multiple_choice" type="radio" />
-                            <label name="multiple_choice">{r ? r : "None"}</label>
-                        </div>
-                    ))}
-                </form>
-                <button onClick={()=>{check()}}>Check answer</button>
-            </> : "Invalid question type" }
-        </div>
+                { questions[questionIndex].type === "text" ? <>
+                    <div className="inputs">
+                        {questions[questionIndex]?.inputs.map((r)=>(
+                            <input key={r.answer} type="text" placeholder={r.placeholder} />
+                        ))}
+                    </div>
+                    <button onClick={()=>{check()}}>Check answers</button>
+                </> : questions[questionIndex].type === "multiple_choice" ? <>
+                    <form className="inputs" name={Math.random()*1000}>
+                        {multipleChoiceOptions.map((r, index)=>(
+                            <div key={index} className="option">
+                                <input name="multiple_choice" type="radio" />
+                                <label name="multiple_choice">{r ? r : "None"}</label>
+                            </div>
+                        ))}
+                    </form>
+                    <button onClick={()=>{check()}}>Check answer</button>
+                </> : "Invalid question type" }
+            </div>
+        </>}
     </>;
 }
--- src/main.jsx
+++ src/main.jsx
@@ -5,14 +5,42 @@ import "./assets/themes.css";
 import "./assets/styles.css";
 import { Toaster } from "react-hot-toast";
 
+import iconSettings from "./assets/icons/cog.svg";
+import iconClose from "./assets/icons/close.svg";
+
 ReactDOM.createRoot(document.getElementById("root")).render(
     <Main />
 );
 
+const availableCategories = [
+    "military",
+    "reconnaissance",
+    "commercial",
+    "training"
+];
+const availableTypes = [
+    "plane",
+    "glider",
+    "helicopter",
+    "drone"
+];
+
+
 function Main() {
     const [score, setScore] = useState({score: 0, total: 0});
+    
+    const [categories, setCategories] = useState(availableCategories);
+    const [types, setTypes] = useState(availableTypes);
+
+    const [settingsOpen, setSettingsOpen] = useState(false);
+    const [showAnswers, setShowAnswers] = useState(true);
+
     function nextScore(correct=false) {
         setScore({ score: correct?score.score+1:score.score, total: score.total+1});
+    }
+    function toggle(list, setList, item) {
+        if (list.includes(item)) setList(list.filter((r)=>r!==item));
+        else setList([...list, item]);
     }
     return <>
         <header className="jsb">
@@ -20,10 +48,41 @@ function Main() {
                 <a className="logo" href="/">ALLEN</a><span>/</span>
                 <p className="title">airrec</p>
             </div>
-            <p className="score">{score.score}/{score.total}</p>
+            <div>
+                <p className="score">{score.score}/{score.total}</p>
+                <button className="settings" onClick={()=>{setSettingsOpen(!settingsOpen)}}>
+                    <img src={ !settingsOpen ? iconSettings : iconClose} />
+                </button>
+            </div>
         </header>
         <div className="content">
-            <App nextScore={nextScore} />
+            { !settingsOpen ? <App nextScore={nextScore} categories={categories} types={types} showAnswers={showAnswers} />
+            : <>
+                <h1 className="title">Settings</h1>
+                <br />
+                <p className="subtitle">Categories</p>
+                <div className="setting checkboxes">
+                    { availableCategories.map((category) => <div key={category}>
+                            <input type="checkbox" name="categories" checked={categories.includes(category)} onChange={()=>{toggle(categories, setCategories, category)}} />
+                            <label>{category}</label>
+                        </div>) }
+                </div>
+
+                <br />
+                <p className="subtitle">Types</p>
+                <div className="setting checkboxes">
+                    { availableTypes.map((type) => <div key={type}>
+                            <input type="checkbox" name="types" checked={types.includes(type)} onChange={()=>{toggle(types, setTypes, type)}} />
+                            <label>{type}</label>
+                        </div>) }
+                </div>
+
+                <br />
+                <div>
+                    <input type="checkbox" name="showanswers" checked={showAnswers} onChange={()=>{setShowAnswers(!showAnswers)}} />
+                    <label>Show correct answers after multiple choice mistakes</label>
+                </div>
+            </> }
         </div>
         <Toaster />
     </>;
